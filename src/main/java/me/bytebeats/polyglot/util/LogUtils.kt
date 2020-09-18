fc1dd3a2c00a2d6251f307ab108b5383816b00eb
panchen <panchen@tigerbrokers.com>
2020-09-18 12:16:41 +0800

    updated to 1.0.1.

--- src/main/java/me/bytebeats/polyglot/util/LogUtils.kt
+++ src/main/java/me/bytebeats/polyglot/util/LogUtils.kt
@@ -1,10 +1,5 @@
 package me.bytebeats.polyglot.util
 
-import com.intellij.notification.NotificationDisplayType
-import com.intellij.notification.NotificationGroup
-import com.intellij.openapi.project.Project
-import com.intellij.openapi.ui.MessageType
-
 /**
  * @author bytebeats
  * @email <happychinapc@gmail.com>
@@ -14,19 +9,8 @@ import com.intellij.openapi.ui.MessageType
  * @description LogUtils notifies users on EventLog window
  */
 
-class LogUtils {
-    companion object {
-        private var project: Project? = null
-        fun init(project: Project) {
-            Companion.project = project
-        }
-
-        fun info(message: String?) {
-            if (message != null && project != null) {
-                NotificationGroup("polyglot", NotificationDisplayType.NONE, true)
-                    .createNotification(message, MessageType.INFO).notify(project)
-            }
-            println(message)
-        }
+object LogUtils {
+    fun info(message: String?) {
+        println(message)
     }
 }
