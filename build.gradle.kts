14216a316193fdb1bae17f072ad79b45a4434485
Chen Pan <chen.pan@goat.com>
2025-06-19 19:02:34 +0800

    regular upgrade with Java 21 and Idea 2025.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.9.22"
-    id("org.jetbrains.intellij") version "1.17.2"
+    id("org.jetbrains.kotlin.jvm") version "2.1.0"
+    id("org.jetbrains.intellij.platform") version "2.5.0"
 }
 
 apply(plugin = "kotlin")
-apply(plugin = "org.jetbrains.intellij")
+apply(plugin = "org.jetbrains.intellij.platform")
 
 group = "io.github.bytebeats"
-version = "1.3.6"
+version = "1.4.0"
 
 repositories {
     maven {
@@ -21,39 +21,55 @@ repositories {
         url = uri("https://maven.aliyun.com/repository/jcenter")
     }
     mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
 }
 
 dependencies {
     api(libs.bundles.jacksonCore)
+    intellijPlatform {
+        create("IC", "2024.2")
+        testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
+
+        // Add necessary plugin dependencies for compilation here, example:
+        // bundledPlugin("com.intellij.java")
+    }
 }
 
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
-intellij {
-    pluginName.set("Polyglot")
-    version.set("2023.2.5")
-    type.set("IC") // Target IDE Platform
+intellijPlatform {
+    pluginConfiguration {
+        ideaVersion {
+            sinceBuild = "242"
+        }
 
-    plugins.set(listOf(/* Plugin Dependencies */))
+        changeNotes = """
+      v1.3.6 project upgrade.<br>
+      v1.4.0 regular upgrade with Java 21 and Idea 2025.<br>
+    """.trimIndent()
+    }
 }
 
 tasks {
     // Set the JVM compatibility versions
     withType<JavaCompile> {
-        sourceCompatibility = "17"
-        targetCompatibility = "17"
+        sourceCompatibility = "21"
+        targetCompatibility = "21"
     }
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "17"
+        kotlinOptions.jvmTarget = "21"
     }
 
     patchPluginXml {
-        sinceBuild.set("230")
-        untilBuild.set("242.*")
+        sinceBuild.set("242")
+//        untilBuild.set("242.*")
 
         changeNotes.set(
             """
       v1.3.6 project upgrade.<br>
+      v1.4.0 regular upgrade with Java 21 and Idea 2025.<br>
       """
         )
     }
